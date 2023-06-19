207c3cb948a02308be9f0092d8ac0df4d94b0800
Mahabubhasan01 <mahabubhasaan@outlook.com>
2023-06-19 12:19:42 +0600

    fitercard adedd

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,16 @@
-import Image from 'next/image'
-import Header from './components/header/Header'
-import HeaderCategory from './components/header/HeaderCategory'
-import styles from './page.module.css'
+import Image from "next/image";
+import FilteredProducts from "./components/FilterProducts/FilteredProducts";
+import Header from "./components/header/Header";
+import HeaderCategory from "./components/header/HeaderCategory";
+import styles from "./page.module.css";
 
-export default function Home({children}) {
+export default function Home({ children }) {
   return (
     <main>
-      <Header/>
-      <HeaderCategory/>
+      <Header />
+      <HeaderCategory />
+      <FilteredProducts />
       {children}
     </main>
-  )
+  );
 }
